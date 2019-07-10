d09f6076275b0924a1daf5470c713c11ef065c0f
kevinpollet <pollet.kevin@gmail.com>
2019-07-10 19:27:24 +0400

    Create interface for Queue data type

---
+++ queue/queue.go
@@ -0,0 +1,18 @@
+/**
+ * Copyright © 2019 kevinpollet <pollet.kevin@gmail.com>`
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE.md file.
+ */
+
+package queue
+
+// Queue Abstract Data Type
+type Queue interface {
+	Clear()
+	Dequeue() (interface{}, error)
+	Enqueue(value interface{})
+	IsEmpty() bool
+	Peek() (interface{}, error)
+	Size() int
+}
