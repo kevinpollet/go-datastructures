cb5fd2b27f88574b9a4e4aee819048e43a01bd84
kevinpollet <pollet.kevin@gmail.com>
2019-08-06 17:44:10 +0400

    Add tests for LinkedListQueue

--- queue/linkedlistqueue/linked_list_queue_test.go
+++ queue/linkedlistqueue/linked_list_queue_test.go
@@ -10,15 +10,116 @@ package linkedlistqueue
 import (
 	"testing"
 
+	"github.com/kevinpollet/go-datastructures/list/doublylinkedlist"
 	queuePkg "github.com/kevinpollet/go-datastructures/queue"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQueueImplementation(test *testing.T) {
+var testCases = []struct {
+	name   string
+	values []int
+}{
+	{"EmptyQueue", []int{}},
+	{"QueueWithOneValue", []int{1}},
+	{"QueueWithTwoValues", []int{1, 2}},
+}
+
+func newLinkedListQueue(values ...int) LinkedListQueue {
+	list := doublylinkedlist.DoublyLinkedList{}
+	for _, value := range values {
+		list.Add(value)
+	}
+	return LinkedListQueue{list: list}
+}
+
+func TestQueueAssertion(test *testing.T) {
 	var queue interface{} = &LinkedListQueue{}
 
-	cast, ok := queue.(queuePkg.Queue)
+	value, ok := queue.(queuePkg.Queue)
 
 	assert.True(test, ok)
-	assert.NotNil(test, cast)
+	assert.NotNil(test, value)
+}
+
+func TestClear(test *testing.T) {
+	for _, testCase := range testCases {
+		test.Run(testCase.name, func(t *testing.T) {
+			queue := newLinkedListQueue()
+			queue.Clear()
+
+			assert.Equal(t, 0, queue.list.Size())
+		})
+	}
+}
+
+func TestDequeue(test *testing.T) {
+	for _, testCase := range testCases {
+		test.Run(testCase.name, func(t *testing.T) {
+			queue := newLinkedListQueue(testCase.values...)
+			value, err := queue.Dequeue()
+
+			switch {
+			case len(testCase.values) == 0:
+				assert.Nil(t, value)
+				assert.Error(t, err)
+			default:
+				assert.Nil(t, err)
+				assert.Equal(t, testCase.values[0], value)
+			}
+		})
+	}
+}
+
+func TestEnqueue(test *testing.T) {
+	for _, testCase := range testCases {
+		test.Run(testCase.name, func(t *testing.T) {
+			queue := newLinkedListQueue()
+			for _, value := range testCase.values {
+				queue.Enqueue(value)
+			}
+
+			assert.Equal(t, len(testCase.values), queue.list.Size())
+			for idx, value := range testCase.values {
+				listValue, _ := queue.list.Get(idx)
+				assert.Equal(t, value, listValue)
+			}
+		})
+	}
+}
+
+func TestIsEmpty(test *testing.T) {
+	for _, testCase := range testCases {
+		test.Run(testCase.name, func(t *testing.T) {
+			queue := newLinkedListQueue(testCase.values...)
+
+			assert.Equal(t, len(testCase.values) == 0, queue.IsEmpty())
+		})
+	}
+}
+
+func TestPeek(test *testing.T) {
+	for _, testCase := range testCases {
+		test.Run(testCase.name, func(t *testing.T) {
+			queue := newLinkedListQueue(testCase.values...)
+			value, err := queue.Peek()
+
+			switch {
+			case len(testCase.values) == 0:
+				assert.Nil(t, value)
+				assert.Error(t, err)
+			default:
+				assert.Equal(t, testCase.values[0], value)
+			}
+		})
+	}
+}
+
+func TestSize(test *testing.T) {
+	for _, testCase := range testCases {
+		test.Run(testCase.name, func(t *testing.T) {
+			queue := newLinkedListQueue(testCase.values...)
+
+			assert.Equal(t, len(testCase.values), queue.Size())
+		})
+	}
 }
